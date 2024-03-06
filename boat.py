51ac9dc4bf1f47a9a586bbeaa78439b011f8e30f
Rishabh Singh <111456553+Rishabhsin@users.noreply.github.com>
2024-03-07 01:32:13 +0530

    Add files via upload

---
+++ boat.py
@@ -0,0 +1,27 @@
+import telebot
+import re
+
+bot = telebot.TeleBot("7010126673:AAHHLWnBG53TEaZxe5YBUFKKHuBe2Zjzy1g")
+
+def text_search(message):
+    substr = message.text.lower()
+    drugs = []
+    with open('wada.txt', 'r') as file:
+        for line in file:
+            if re.search(r'\b{}\b'.format(substr), line.lower()):
+                drugs.append(line.rstrip())
+    if drugs:
+        return "The Drug you have mentioned is prohibited by WADA.\n\nPlease Do Consult a Doctor before using it. \n-----------------------------------------------\nHere is what I found...\n\n" + "\n".join(drugs)
+    else:
+        return "The drug you have mentioned, is not in the list of Wada's prohibited. Drugs\n\nPlease Do Consult a Doctor before using it."
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "WaDa prohibited list search. Please enter the drug name")
+
+@bot.message_handler(func=lambda message: len(message.text) > 3)
+def get_text_messages(message):
+    response = text_search(message)
+    bot.reply_to(message, response or "No valid drug name provided")
+
+bot.infinity_polling()
